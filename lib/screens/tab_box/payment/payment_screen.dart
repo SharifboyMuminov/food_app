5568dba502b676cfcd81bd230c1b26861a7f85ec
Sharifjon <sharifboymuminov@gmail.com>
2024-07-07 07:02:36 +0500

    the end work :)

--- lib/screens/tab_box/payment/payment_screen.dart
+++ lib/screens/tab_box/payment/payment_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_app/screens/tab_box/payment/widget/card_info_input.dart';
 import 'package:food_app/screens/tab_box/payment/widget/card_item.dart';
 import 'package:food_app/screens/tab_box/payment/widget/open_camera_button.dart';
